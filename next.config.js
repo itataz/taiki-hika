12d5a331a0d512c4a22696fc2219499e50be00be
T <your-email@example.com>
2025-06-12 20:55:30 +0900

    Remove i18n config to fix routing

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // PWA対応の準備
-  i18n: {
-    locales: ['en', 'ja'],
-    defaultLocale: 'en',
-  },
 }
 
 module.exports = nextConfig
